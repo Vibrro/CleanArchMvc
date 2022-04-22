ab64dcc8ec753ef6997eca209d21f48f887fb1fa
Diego <contato.datakuri@gmail.com>
2022-04-22 16:47:14 -0300

    CQRS part 2

--- CleanArchMvc.Application/Products/Commands/ProductUpdateCommand.cs
+++ CleanArchMvc.Application/Products/Commands/ProductUpdateCommand.cs
@@ -1,15 +1,7 @@
-﻿using CleanArchMvc.Domain.Entities;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanArchMvc.Application.Products.Commands
+﻿namespace CleanArchMvc.Application.Products.Commands
 {
     public class ProductUpdateCommand : ProductCommand
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
     }
 }
